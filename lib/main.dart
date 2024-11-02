71c895b07e362eeb97cda5a2453fa351b2b60d0c
HossamAhmadMohamed <ugs.141252@ci.suez.edu.eg>
2024-11-02 15:07:18 +0200

    fire_base_setup_donee

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:commerce_app/my_app.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: 'AIzaSyCOhEziP_CyyxxwU1pyqHZwa9TaaaGIGlU',
+          appId: '1:892316346093:android:00cd5f5761829622d24ee4',
+          messagingSenderId: '892316346093',
+          projectId: 'store-app-e25e2'));
+
   runApp(const MyApp());
 }
-
-
-
- 
-   
