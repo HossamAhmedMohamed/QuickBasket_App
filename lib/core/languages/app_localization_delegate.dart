e145d9ade12a667d0351d33e7e0c0dcdee2cf040
HossamAhmadMohamed <ugs.141252@ci.suez.edu.eg>
2024-11-18 15:10:06 +0200

    localization setup done

---
+++ lib/core/languages/app_localization_delegate.dart
@@ -0,0 +1,24 @@
+import 'package:commerce_app/core/languages/app_localization.dart';
+import 'package:flutter/material.dart';
+
+class AppLocalizationsDelegate extends LocalizationsDelegate<AppLocalizations> {
+  const AppLocalizationsDelegate();
+
+  // Override method to check if a given locale is supported by the app.
+  @override
+  bool isSupported(Locale locale) {
+    return ['en', 'ar'].contains(locale.languageCode);
+  }
+
+  // Override method to load the AppLocalizations for a given locale.
+  @override
+  Future<AppLocalizations> load(Locale locale) async {
+    final appLocalizations = AppLocalizations(locale);
+    await appLocalizations.load();
+    return appLocalizations;
+  }
+
+  // Override method to determine whether the localization should be reloaded.
+  @override
+  bool shouldReload(LocalizationsDelegate<AppLocalizations> old) => false;
+}
