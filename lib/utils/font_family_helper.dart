ee98f410e24e797189a1a988f196c09403507444
HossamAhmadMohamed <ugs.141252@ci.suez.edu.eg>
2024-11-18 03:44:13 +0200

    font family done

---
+++ lib/utils/font_family_helper.dart
@@ -0,0 +1,12 @@
+
+// ignore_for_file: eol_at_end_of_file
+
+class FontFamilyHelper {
+  static const String cairoArabic = 'Cairo';
+  static const String poppinsEnglish = 'Poppins';
+
+  //TODO: switch between araic and english
+}
+
+
+
