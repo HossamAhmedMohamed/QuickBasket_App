483c693804eedbf5fb0c3ef8bc671dbb0add0570
HossamAhmadMohamed <ugs.141252@ci.suez.edu.eg>
2024-11-18 15:52:54 +0200

    lang , color and assets done

--- lib/utils/app_images.dart
+++ lib/utils/app_images.dart
@@ -2,9 +2,33 @@
 class Assets {
   Assets._();
   
+  /// Assets for imagesAdminCategoriesDrawer
+  /// assets/images/admin/categories_drawer.png
+  static const String imagesAdminCategoriesDrawer = "assets/images/admin/categories_drawer.png";
+
+  /// Assets for imagesAdminDrawerImage
+  /// assets/images/admin/drawer_image.png
+  static const String imagesAdminDrawerImage = "assets/images/admin/drawer_image.png";
+
+  /// Assets for imagesAdminProductsDrawer
+  /// assets/images/admin/products_drawer.png
+  static const String imagesAdminProductsDrawer = "assets/images/admin/products_drawer.png";
+
+  /// Assets for imagesAdminUsersDrawer
+  /// assets/images/admin/users_drawer.png
+  static const String imagesAdminUsersDrawer = "assets/images/admin/users_drawer.png";
+
+  /// Assets for imagesCoreDarkTestImage
+  /// assets/images/core/dark_test_image.png
+  static const String imagesCoreDarkTestImage = "assets/images/core/dark_test_image.png";
+
   /// Assets for imagesCoreEmptyScreen
   /// assets/images/core/empty_screen.png
   static const String imagesCoreEmptyScreen = "assets/images/core/empty_screen.png";
+
+  /// Assets for imagesCoreLightTestImage
+  /// assets/images/core/light_test_image.png
+  static const String imagesCoreLightTestImage = "assets/images/core/light_test_image.png";
 
   /// Assets for imagesCoreNoNetwork
   /// assets/images/core/no_network.jpg
@@ -14,7 +38,92 @@ class Assets {
   /// assets/images/core/page_under_build.jpg
   static const String imagesCorePageUnderBuild = "assets/images/core/page_under_build.jpg";
 
-  /// Assets for imagesCoreSplash
-  /// assets/images/core/splash.jpg
-  static const String imagesCoreSplash = "assets/images/core/splash.jpg";
+  /// Assets for imagesCoreSplashAndroid12
+  /// assets/images/core/splash_android_12.png
+  static const String imagesCoreSplashAndroid12 = "assets/images/core/splash_android_12.png";
+
+  /// Assets for imagesCoreSplashIosAndroid11
+  /// assets/images/core/splash_ios_android_11.png
+  static const String imagesCoreSplashIosAndroid11 = "assets/images/core/splash_ios_android_11.png";
+
+  /// Assets for imagesCustomerBigNavBarDark
+  /// assets/images/customer/big_nav_bar_dark.png
+  static const String imagesCustomerBigNavBarDark = "assets/images/customer/big_nav_bar_dark.png";
+
+  /// Assets for imagesCustomerBigNavBarLight
+  /// assets/images/customer/big_nav_bar_light.png
+  static const String imagesCustomerBigNavBarLight = "assets/images/customer/big_nav_bar_light.png";
+
+  /// Assets for imagesCustomerHomeBgDark
+  /// assets/images/customer/home_bg_dark.png
+  static const String imagesCustomerHomeBgDark = "assets/images/customer/home_bg_dark.png";
+
+  /// Assets for imagesCustomerHomeBgLight
+  /// assets/images/customer/home_bg_light.png
+  static const String imagesCustomerHomeBgLight = "assets/images/customer/home_bg_light.png";
+
+  /// Assets for imagesCustomerUser
+  /// assets/images/customer/user.png
+  static const String imagesCustomerUser = "assets/images/customer/user.png";
+
+  /// Assets for imagesIconsAppIcon
+  /// assets/images/icons/app_icon.jpg
+  static const String imagesIconsAppIcon = "assets/images/icons/app_icon.jpg";
+
+  /// Assets for imagesIconsPlayStore512
+  /// assets/images/icons/play_store_512.png
+  static const String imagesIconsPlayStore512 = "assets/images/icons/play_store_512.png";
+
+  /// Assets for imagesSvgArrowBack
+  /// assets/images/svg/arrow_back.svg
+  static const String imagesSvgArrowBack = "assets/images/svg/arrow_back.svg";
+
+  /// Assets for imagesSvgBuildDeveloper
+  /// assets/images/svg/build_developer.svg
+  static const String imagesSvgBuildDeveloper = "assets/images/svg/build_developer.svg";
+
+  /// Assets for imagesSvgBuildVersion
+  /// assets/images/svg/build_version.svg
+  static const String imagesSvgBuildVersion = "assets/images/svg/build_version.svg";
+
+  /// Assets for imagesSvgCarShop
+  /// assets/images/svg/car_shop.svg
+  static const String imagesSvgCarShop = "assets/images/svg/car_shop.svg";
+
+  /// Assets for imagesSvgCategoriesTapIcon
+  /// assets/images/svg/categories_tap_icon.svg
+  static const String imagesSvgCategoriesTapIcon = "assets/images/svg/categories_tap_icon.svg";
+
+  /// Assets for imagesSvgDarkMode
+  /// assets/images/svg/dark_mode.svg
+  static const String imagesSvgDarkMode = "assets/images/svg/dark_mode.svg";
+
+  /// Assets for imagesSvgFavouriteTabIcon
+  /// assets/images/svg/favourite_tab_icon.svg
+  static const String imagesSvgFavouriteTabIcon = "assets/images/svg/favourite_tab_icon.svg";
+
+  /// Assets for imagesSvgHomeTabIcon
+  /// assets/images/svg/home_tab_icon.svg
+  static const String imagesSvgHomeTabIcon = "assets/images/svg/home_tab_icon.svg";
+
+  /// Assets for imagesSvgLanguage
+  /// assets/images/svg/language.svg
+  static const String imagesSvgLanguage = "assets/images/svg/language.svg";
+
+  /// Assets for imagesSvgLogout
+  /// assets/images/svg/logout.svg
+  static const String imagesSvgLogout = "assets/images/svg/logout.svg";
+
+  /// Assets for imagesSvgPlayStore512
+  /// assets/images/svg/play_store_512.png
+  static const String imagesSvgPlayStore512 = "assets/images/svg/play_store_512.png";
+
+  /// Assets for imagesSvgProfileTabIcon
+  /// assets/images/svg/profile_tab_icon.svg
+  static const String imagesSvgProfileTabIcon = "assets/images/svg/profile_tab_icon.svg";
+
+  /// Assets for imagesSvgSearch
+  /// assets/images/svg/search.svg
+  static const String imagesSvgSearch = "assets/images/svg/search.svg";
 }
+
